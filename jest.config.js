56eeef4df4190d65a7610a191e2d9569537695a4
VikrantKS <96419531+VikrantKS@users.noreply.github.com>
2022-03-16 11:34:56 +0530

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const config = {
+    
+modulePathIgnorePatterns: [
+    "/lib/",
+    "/node_modules/"
+  ],
+  preprocessorIgnorePatterns: [
+    "/node_modules/"
+  ],
+  scriptPreprocessor: "scripts/jest/preprocessor.js",
+  setupFiles: [
+    "scripts/jest/environment.js"
+  ],
+
+}
